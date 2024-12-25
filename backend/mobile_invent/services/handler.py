4caf0b4bca9439797b1b8625707eb9e251629a2d
Inbasu <just.me.juncy@gmail.com>
2024-12-25 09:09:28 +0300

    new file:   backend/mobile_invent/services/giveaway.py 	modified:   backend/mobile_invent/services/handler.py 	new file:   backend/mobile_invent/services/send.py 	modified:   backend/mobile_invent/services/takeback.py

--- backend/mobile_invent/services/handler.py
+++ backend/mobile_invent/services/handler.py
@@ -1,31 +1,44 @@
+import base64
 import time
 from abc import ABC, abstractmethod
 from logging import getLogger
+from typing import Any
 
 import requests
 
 
+class EreqChangeError(Exception):
+    pass
+
+class ITREQError(Exception):
+    pass
+
 class Handler(ABC):
     logger = getLogger("mobile")
+    user: dict
 
-    @classmethod
-    def get_field_by_name(cls, fields: list[dict], name: str):
+    @staticmethod
+    def get_field_by_name(fields: list[dict], name: str):
         for field in fields:
             if field["name"] == name:
                 return field
         return {}
 
-    @classmethod
-    def get_user(cls, email: str) -> int:
-        resp = requests.post('http://127.0.0.1:8000/iql', json={"scheme": 2, "iql":f"objectTypeId = 57 AND Email = {email}"}).json()
+    @staticmethod
+    def get_label_of_field(field: dict) -> Any:
+        return field.get("values", [{}])[0].get("label")
+
+
+    def get_user(self) -> int:
+        resp = requests.post('http://127.0.0.1:8000/iql', json={"scheme": 2, "iql":f"objectTypeId = 57 AND Email = {self.user.get('email')}"}).json()
         try:
             return resp[0].get('id', 0)
         except IndexError:
             return 0
     
-    @classmethod
-    def obj_link(cls, id: int) -> str:
+    def obj_link(self, id: int) -> str:
         return f'https://jirainvent.metro-cc.ru/secure/insight/assets/IN-{id}'
+
 
 
     @staticmethod
@@ -33,7 +46,7 @@ class Handler(ABC):
         def wrapper(cls, operation_id: str, user, item, action, *args, **kwargs):
             if user_id := cls.get_user(user['email']):
                 cls.logger.info(f'{operation_id}: {user['email']} {action.upper()} {item["label"]}')
-                update = func(cls, item=item, user=user_id, operation_id=operation_id ,*args, **kwargs)
+                update = func(cls, item=item, hws_user_id=user_id, operation_id=operation_id ,*args, **kwargs)
                 if not update.get("error", False):
                     time.sleep(3)
                     requests.post('http://127.0.0.1:8000/update', json={"scheme": 1, "object_type_id": 8, "object_id": item["id"],
@@ -43,7 +56,25 @@ class Handler(ABC):
         return wrapper
 
 
-    @classmethod
     @abstractmethod
-    def handle(cls, item, user, **kwargs) -> dict:
+    def handle(self) -> dict:
+        pass
+
+    def jira_add_attachment(self, itreq: str, file):
+        source = base64.b64encode(file.read()).decode("utf-8")
+        res = requests.post('http://127.0.0.1:8000/add_attachment', json={"project": "it", "issue": itreq, "name": file.name, "source": source})
+        if res.status_code == 200:
+            return None
+        raise ITREQError("Проблема при добавлении к Jira реквесту файла")
+
+
+    def get_component(self):
+        components = {"ipad": "iPad",
+                      "laptop": "Laptop",
+                     }
+        return ''
+
+
+
+    def insight_add_attachment(self, id, file):
         pass
--- backend/mobile_invent/services/takeback.py
+++ backend/mobile_invent/services/takeback.py
@@ -1,58 +1,58 @@
-import base64
 import datetime
-from typing import Any
 
 import requests
 
-from mobile_invent.services.handler import Handler
+from mobile_invent.services.handler import EreqChangeError, Handler, ITREQError
 
-
-class EreqChangeError(Exception):
-    pass
-
-class ITREQError(Exception):
-    pass
 
 class TakebackHandler(Handler):
 
-    @classmethod
+    def __init__(self, item, user, hw_user_id, store, operation_id, file) -> None:
+        self.item = item
+        self.ereq = item.get('joined', [{}])[0]
+        self.ereq_id = self.ereq.get('id')
+        self.user = user
+        self.hw_user_id = hw_user_id
+        self.store = store
+        self.file = file
+        self.operation_id = operation_id
+        
+
+
     @Handler.hw_user_update
-    def handle(cls, item: dict[str, Any],user_email: str, user_id: int, operation_id: str, file, store, **kwargs) -> dict[str, str|dict]:
-        ereqs = item.get('joined', False)
-        if not ereqs or not ereqs[0].get("id", False) or ereqs[0].get("Дата сдачи", False):
-            msg = f"{operation_id}: Нет открытого Erequest'а для {item["label"]}"
-            cls.logger.error(msg) 
+    def handle(self) -> dict[str, str|dict]:
+        if not self.ereq or self.ereq.get("Дата сдачи", False):
+            msg = f"{self.operation_id}: Нет открытого Erequest'а для {self.item["label"]}"
+            self.logger.error(msg) 
             return {"result": "", "error": msg}
-        ereq_id = ereqs[0].get("id")
-        itreq = [path for path in cls.get_field_by_name(ereqs[0].get("attrs", {}), "Реквест").get("values", [{}])[0].get("label").split('/') if path][-1]
-        inv = cls.get_field_by_name(item['attrs'], "INV No").get('values', [{}])[0].get("label")
-        name = f'[{itreq}][{inv}] {item["label"]}'
+        itreq = [path for path in Handler.get_field_by_name(self.ereq.get("attrs", {}), "Реквест").get("values", [{}])[0].get("label").split('/') if path][-1]
+        inv = Handler.get_field_by_name(self.item['attrs'], "INV No").get('values', [{}])[0].get("label")
+        ereq_id = self.ereq[0].get('id'),
         try:
-            ereq = cls.change_erequest(ereq_id, user_id, name)
-            cls.logger.info(f'{operation_id}: Erequest {cls.obj_link(ereq_id)} отредактирован')
-            cls.insight_add_attachment(ereq_id, file)
-            cls.logger.info(f'{operation_id}: К Erequest`у добавлен файл')
-            jreq = cls.jira_create_req(user_email, 
-                                       cls.get_field_by_name(store['attrs'], "Jira Issue Location").get("values", [{}])[0].get('label', ''), 
-                                       cls.obj_link(ereq_id), cls.obj_link(item['id']), cls.get_component(item))
-            cls.logger.info(f'{operation_id}: Заявка https://jira.metro-cc.ru/browse/{123} создана')
-            cls.jira_add_attachment(itreq=jreq, file=file)
-            cls.logger.info(f'{operation_id}: Файл {file.name} добавлен к заявке')
-            cls.logger.info(f'{operation_id}: Оборудование успешно сдано {cls.obj_link(item['id'])}')
+            ereq = self.change_erequest(name=f'[{itreq}][{inv}] {self.item["label"]}')
+            self.logger.info(f'{self.operation_id}: Erequest {self.obj_link(self.ereq_id)} отредактирован')
+            self.insight_add_attachment(ereq_id, self.file)
+            self.logger.info(f'{self.operation_id}: К Erequest`у добавлен файл')
+            jreq = self.jira_create_req(self.user.get("email"), 
+                                       Handler.get_field_by_name(self.store['attrs'], "Jira Issue Location").get("values", [{}])[0].get('label', ''), 
+                                       self.obj_link(self.ereq_id), self.obj_link(self.item['id']), self.get_component())
+            self.logger.info(f'{self.operation_id}: Заявка https://jira.metro-cc.ru/browse/{123} создана')
+            self.jira_add_attachment(itreq=jreq, file=self.file)
+            self.logger.info(f'{self.operation_id}: Файл {self.file.name} добавлен к заявке')
+            self.logger.info(f'{self.operation_id}: Оборудование успешно сдано {self.obj_link(self.item['id'])}')
             return {"result": ereq, "error": ""}
         except (ITREQError, EreqChangeError) as e:
-            cls.logger.error(f'{operation_id}: {e}')
+            self.logger.error(f'{self.operation_id}: {e}')
             return {"result": "", "error": str(e)}
 
 
-    @classmethod
-    def change_erequest(cls, ereq_id, user_id, name: str):
+    def change_erequest(self, name: str):
         cur_date = datetime.date.today()
-        ereq_change_response = requests.post('http://127.0.0.1:8000/update', json={'scheme': 1, "object_type_id": 78, "object_id": ereq_id,
+        ereq_change_response = requests.post('http://127.0.0.1:8000/update', json={'scheme': 1, "object_type_id": 78, "object_id": self.ereq_id,
                                   "attrs":{
                                         819: [name],                                                              # name
                                         827: [f'{cur_date.day}/{cur_date.month}/{str(cur_date.year)[-2:]}'],      # Дата сдачи
-                                        831: [user_id],                                                         #Кто принял,
+                                        831: [self.hw_user_id],                                                         #Кто принял,
             }})
         if ereq_change_response.status_code == 200:
             return ereq_change_response.json()
@@ -60,8 +60,7 @@ class TakebackHandler(Handler):
 
 
         
-    @classmethod
-    def jira_create_req(cls, email, location, ereq, item, component):
+    def jira_create_req(self, email, location, ereq, item, component):
         jreq = requests.post('', json={
             "summary": f"Сдача мобильного оборудования ТЦ {location.split()[0]}",
             "description": f'Пользователь {email} создал задачу по сдаче оборудования\n{item}\nБланк {ereq}',
@@ -73,22 +72,8 @@ class TakebackHandler(Handler):
             return jreq.json()
         raise ITREQError('Проблема при создании заявки в Jira')
 
-    @classmethod
-    def jira_add_attachment(cls, itreq: str, file):
-        source = base64.b64encode(file.read()).decode("utf-8")
-        res = requests.post('http://127.0.0.1:8000/add_attachment', json={"project": "it", "issue": itreq, "name": file.name, "source": source})
-        if res.status_code == 200:
-            return None
-        raise ITREQError("Проблема при добавлении к Jira реквесту файла")
-
-    @classmethod
-    def insight_add_attachment(cls, ereq_id, file):
-        pass
 
 
-    @classmethod
-    def get_component(cls, item):
-        components = {"ipad": "iPad",
-                      "laptop": "Laptop",
-                     }
-        return ''
+
+
+
